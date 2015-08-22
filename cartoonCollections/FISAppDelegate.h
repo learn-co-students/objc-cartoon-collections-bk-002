3f30381e2033b4e7ea1751a6c94edc0161bf7b2b
azmuth94 <dannyaksenov@gmail.com>
2015-08-22 18:00:04 -0400

    dat premiummm cheeeeeese

--- cartoonCollections/FISAppDelegate.h
+++ cartoonCollections/FISAppDelegate.h
@@ -12,10 +12,11 @@
 
 @property (strong, nonatomic) UIWindow *window;
 
-/**
- 
- * Declare your methods here.
- 
- */
+- (NSString *)stringByRollCallingDwarfsInArray:(NSArray *)dwarfs;
+- (NSArray *)arrayOfPlaneteerShoutsFromArray:(NSArray *)powers;
+- (NSString *)summonCaptainPlanetWithPowers:(NSArray *)powers;
+- (NSString *)firstPremiumCheeseInStock:(NSArray *)cheeseInStock premiumCheeseNames:(NSArray *)premiumCheeseNames;
+- (NSArray *)arrayByConvertingMoneyBagsIntoPaperBills:(NSArray *)moneyBags;
+
 
 @end
